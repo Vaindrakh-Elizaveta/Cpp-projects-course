18c9b94e68a22613d62a417bc0b2f726948ac5b6
Вайндрах Елизавета <liza_vaindrakh@mail.ru>
2025-02-09 20:01:44 +0300

    Написала функцию поиска корней

--- Lab1/laba1.cpp
+++ Lab1/laba1.cpp
@@ -8,6 +8,27 @@ void get_coefficients(double &a, double &b, double &c)
 	std::cin >> a >> b >> c;
 }
 
+void find_roots(double a, double b, double c, double& root1, double& root2, bool& HasRealRoots)
+{
+	double D = b * b - 4 * a * c;
+
+	if (D > 0)
+	{
+		root1 = (-b + std::sqrt(D)) / (2 * a);
+		root2 = (-b - std::sqrt(D)) / (2 * a);
+		HasRealRoots = true;
+	}
+	else if (D == 0)
+	{
+		root1 = root2 = -b / (2 * a);
+		HasRealRoots = true;
+	}
+	else
+	{
+		HasRealRoots = false;
+	}
+}
+
 int main()
 {
 	setlocale(LC_ALL, "Russian");
@@ -15,6 +36,7 @@ int main()
 	bool HasRealRoots = true;
 
 	get_coefficients(a, b, c);
+	find_roots(a, b, c, root1, root2, HasRealRoots);
 
 	return 0;
 }

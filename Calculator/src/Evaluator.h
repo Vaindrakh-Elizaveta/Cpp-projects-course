8b86a9a3c22095cb2a030f6bea25c7aa089d84fb
Вайндрах Елизавета <liza_vaindrakh@mail.ru>
2025-12-03 18:01:17 +0300

    Удалила файлы первой лабораторной

--- Calculator/src/Evaluator.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include"Token.h"
-#include"FunctionRegistry.h"
-#include<vector>
-
-class Evaluator {
-public:
-	static double eval(const std::vector<RpnItem>& rpn, const FunctionRegistry& reg);
-};

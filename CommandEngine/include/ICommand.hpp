42a4b9b2c1455fe848c64bf2f0093fe88880b53d
Вайндрах Елизавета <liza_vaindrakh@mail.ru>
2025-12-03 18:44:54 +0300

    Add ostream operator<< for ArgValue (std::variant)

--- CommandEngine/include/ICommand.hpp
+++ CommandEngine/include/ICommand.hpp
@@ -1,14 +1,33 @@
 #pragma once
+#include<string>
 #include <vector>
-#include <string>
 #include <variant>
 #include <utility>
+#include <ostream>
+#include <type_traits>
 
 using ArgValue = std::variant<std::monostate, int, double, std::string, bool>;
 using ArgList = std::vector<std::pair<std::string, ArgValue>>;
 
+//Удобный вывод ArgValue в std::cout
+inline std::ostream& operator<<(std::ostream& os, const ArgValue& v)
+{
+    std::visit([&](const auto& value) {
+        using T = std::decay_t<decltype(value)>;
+        if constexpr (std::is_same_v<T, std::monostate>) {
+            os << "<void>";    //для функций с типом возврата void
+        }
+        else {
+            os << value;
+        }
+        }, v);
+    return os;
+}
+
 class ICommand {
 public:
     virtual ~ICommand() = default;
+
+    //Каждый класс-команда обязан реализовать этот метод
     virtual ArgValue execute(const ArgList& args) = 0;
 };
